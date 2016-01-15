d3bf93d042fa9d34de02be4fd84d06c345262f8e
Nick Reid <nreid26@uwo.ca>
2016-01-15 18:24:06 -0500

    -created identifiable library and tests

---
+++ lib/identity.dart
@@ -0,0 +1,12 @@
+library identity;
+
+///The interface of an identifiable object. When used my client code, distinct objects with the same identity should not coexist within the same structure.
+abstract class Identifiable {
+  //Methods
+  int get identity;
+
+  int get hashCode => identity;
+  bool operator==(Identifiable other) => (other == null) ? false : identity == other.identity;
+
+  String toString() => '${runtimeType.toString()}: $identity';
+}
---
+++ test/identity_tests.dart
@@ -0,0 +1,43 @@
+import 'package:test/test.dart';
+import 'package:partyshark_server/identity.dart';
+
+class ConcreteIdentifiable extends Object with Identifiable {
+  //Data
+  final int identity, nonIdentity;
+
+  //Constructor
+  ConcreteIdentifiable(this.identity, this.nonIdentity);
+}
+
+int main() {
+  test('Identity defines equality on identifiables', () {
+    final ConcreteIdentifiable
+        a = new ConcreteIdentifiable(0, 0),
+        b = new ConcreteIdentifiable(0, 1),
+        c = new ConcreteIdentifiable(1, 0),
+        d = new ConcreteIdentifiable(1, 1);
+
+    expect(a, equals(b));
+    expect(c, equals(d));
+
+    expect(a, isNot(equals(c)));
+    expect(a, isNot(equals(d)));
+
+    expect(b, isNot(equals(c)));
+    expect(b, isNot(equals(d)));
+  });
+
+  test('Identity defines hashcodes on identifiables', () {
+    final ConcreteIdentifiable
+      a = new ConcreteIdentifiable(0, 0),
+      b = new ConcreteIdentifiable(0, 1);
+
+    expect(a.hashCode, equals(b.hashCode));
+  });
+
+  test('Identifiables have their identity in their toString() representation', () {
+    final ConcreteIdentifiable a = new ConcreteIdentifiable(1010, 9);
+
+    expect(a.toString(), contains('1010'));
+  });
+}
