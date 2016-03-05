a6b9f5aaef137427f5b325070d7071a3fb9cc326
Nick Reid <nreid26@uwo.ca>
2016-03-05 08:22:45 -0500

    -began defining main

--- bin/main.dart
+++ bin/main.dart
@@ -1,6 +1,27 @@
-// Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
+import 'dart:io';
 
-main(List<String> arguments) {
+import 'package:partyshark_server/pseudobase/pseudobase.dart';
+import 'package:partyshark_server/src/controllers/controllers.dart';
+import 'package:partyshark_server/src/entities/entities.dart';
+import 'package:partyshark_server/signpost/signpost.dart';
 
+class RootController extends MisrouteController { }
+
+main(List<String> arguments) async {
+  model = new Datastore([Ballot, Party, PlayerTransfer, Playthrough, SettingsGroup, Song, User]);
+
+  var definition = {
+    'parties': [partiesController, {
+      CustomKey.PartyCode: [partyController, {
+        'playlist': [playlistController, {
+          CustomKey.PlaythroughCode: playthroughController
+        }]
+      }]
+    }]
+  };
+
+  (await HttpServer.bind(InternetAddress.ANY_IP_V4, int.parse(arguments[1])))
+      .listen(
+          new Router(arguments[0], new RootController(), definition).routeRequest
+      );
 }
