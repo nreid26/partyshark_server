9357726c66e308567d984c679de362c452c67cee
Nick Reid <nreid26@uwo.ca>
2016-03-15 23:39:07 -0400

    reorganized launch

--- bin/launch_server.dart
+++ bin/launch_server.dart
@@ -1,12 +1,12 @@
 import 'dart:io';
-import 'dart:async' show Future;
+import 'dart:async';
 
 import 'package:partyshark_server/src/controllers/controllers.dart';
 import 'package:partyshark_server/src/randomization_service/randomization_service.dart' as rand_serve show ready;
 import 'package:partyshark_server/signpost/signpost.dart';
 import 'package:partyshark_server/src/model/model.dart' as model;
 
-final definition = {
+final _definition = {
   'parties': [Controller.Parties, {
     Key.PartyCode: [Controller.Party, {
       'playlist': [Controller.Playlist, {
@@ -24,24 +24,13 @@ final definition = {
   }]
 };
 
-Future launchApiServer(String baseUri, int port) async {
-  var server, router;
+Future<StreamSubscription> launchApiServer(String baseUri, int port) async {
+  await rand_serve.ready;
 
-  try {
-    await rand_serve.ready;
+  final router = new Router(baseUri, new MisrouteController(), _definition);
+  final server = await HttpServer.bind(InternetAddress.ANY_IP_V4, port);
 
-    router = new Router(baseUri, new MisrouteController(), definition);
-    server = await HttpServer.bind(InternetAddress.ANY_IP_V4, port);
-
-    model.logger.info('API server launched');
-  }
-  catch (e, trace) {
-    model.logger.severe('API server failed to launch', e, trace);
-    return;
-  }
-
-
-  await for (Future res in server.map(router.routeRequest)) {
+  final sub = server.map(router.routeRequest).listen((Future res) async {
     try { await res; }
     catch (e, trace) {
       model.logger.severe('Uncaught throw during API request', e, trace);
@@ -51,5 +40,8 @@ Future launchApiServer(String baseUri, int port) async {
         rethrow;
       }
     }
-  }
+  });
+
+  model.logger.info('API server launched');
+  return sub;
 }
--- bin/main.dart
+++ bin/main.dart
@@ -1,25 +1,53 @@
+import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:partyshark_server/src/model/model.dart' as model;
 
 import './launch_server.dart';
+import './arg_parsing.dart';
 
 
-void main(List<String> args) {
-  final int logIndex = args.indexOf('-l');
-  prepareLogger((logIndex >= 0) ? int.parse(args[logIndex + 1]) : 4);
+Future main(List<String> allArgs) async {
+  final ArgManager args = new ArgManager(allArgs);
 
-  launchApiServer(args[0], int.parse(args[1]));
+  prepareLogger(args.logLevel);
 
+  final serverSub = await launchApiServer(args.baseUri, args.port);
+
+  listenOnStdin((line, sub) {
+    if (line.toLowerCase() == 'exit') {
+      sub.cancel();
+      serverSub.cancel();
+
+      print('Sever shutdown');
+      print('Hit enter to continue');
+    }
+  });
 }
 
-
 void prepareLogger(int levelIndex) {
+  final log = new File('log.txt');
+
+  if (log.existsSync()) {
+    log.delete();
+  }
+
   model.logger
       ..level = model.Level.LEVELS[levelIndex]
       ..onRecord.listen((rec) {
         String message = '${rec.level} ${rec.message}';
         if (rec.error != null) { message += rec.error.toString(); }
-        message += '\n';
+        message += '\r\n';
 
-        print(message);
+        log.writeAsStringSync(message, mode: FileMode.APPEND);
       });
+}
+
+void listenOnStdin(dynamic handler(String line, StreamSubscription sub)) {
+  var sub;
+  sub = stdin
+      .transform(UTF8.decoder)
+      .transform(new LineSplitter())
+      .listen((line) { handler(line, sub); });
 }
